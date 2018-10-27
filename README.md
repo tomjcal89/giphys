e12cce3344eeb23047dd1fa4f4eedd7aceae41e3
Thomas J Calhoun <thomascalhoun@Thomass-MBP.attlocal.net>
2018-10-26 23:11:26 -0700

    added final changes for the night

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # giphys
+An application  that generates Gighys and you are able to play and pause them!
--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,6 @@
 
-//array of food
-var topics = ["ramen", "pizza", "hamburger", "fried chicken", "steak", "chips", "french fries", "apple", "cheese", "chocolate"];
+//array of person
+var topics = ["oprah", "luke bryan", "simon cowell", "melissa mcarthy", "jennifer aniston", "bradly cooper", "ellen degenerous", "beyonce", "steph curry", "the rock"];
 //displaying info onto the page
 function displayInfo() {
 //naming the url
@@ -23,25 +23,31 @@ $.ajax({
     var rating = response.data[i].rating
 
     //setting a variable for the image of the gif
-    var foodURL = response.data[i].images.fixed_height.url;
-
+    var personURL = response.data[i].images.fixed_height.url;
+    var stillImg = response.data[i].images.fixed_width_still.url;
    //creating div for the gif
-    var gifDiv = $("<div>")
+    var gifDiv = $("<div class='col-lg-4 col-sm-6'>");
    
    //creating an image tag to place the gif in
-    var foodImage = $("<img>");
+    var personImage = $("<img>").attr("src", stillImg);
+    
+
+    personImage.addClass("image")
 
     //creating a paragraph tag to place the rating in and putting it on the page
     var p = $("<p>").text("Rating: " + rating)
     
     //taking the image tag and putting the image of thge gif in it
-    foodImage.attr("src", foodURL);
+    personImage.attr("data-state", "still");
+    personImage.attr("data-animate", personURL);
+    personImage.attr("data-still", stillImg);
+    
 
-    //if the image doesnt populate, "Food Image" will take place on the page
-    foodImage.attr("alt", "Food Image");
+    //if the image doesnt populate, "person Image" will take place on the page
+    personImage.attr("alt", "person Image");
 
     //appending the gifDiv to the page with both the paragraph and the image
-    gifDiv.append(p, foodImage);
+    gifDiv.append(p, personImage);
 
     //selecting the div in which the image will pupulate on the page
     $("#appearGif").prepend(gifDiv)
@@ -54,7 +60,7 @@ $.ajax({
 function renderButtons(){
   
   //emptying the buttons
-  $("#foodButton").empty();
+  $("#personButton").empty();
 //looping through the topics
   for(i = 0; i < topics.length; i++){
     //dynamically creating a button
@@ -66,20 +72,40 @@ function renderButtons(){
   //giving each button a name of each string in the array
   button.text(topics[i]);
   //appending the button to the screen
-  $("#foodButton").append(button);
+  $("#personButton").append(button);
   }
 };
 //creating a funciton to create a button when pressing on the add button on screen
 $("#add").on("click", function(event){
   event.preventDefault();
 //
-  var addFood = $("#input").val().trim();
-  topics.push(addFood)
+  var addperson = $("#input").val().trim();
+  topics.push(addperson)
   renderButtons()
 
   
 });
+
+//creating animate function
+
+function gifAnimate() {
+	var current = $(this).attr("data-state");
+	var still = $(this).attr("data-still");
+	var animate = $(this).attr("data-animate");
+
+	if (current === "still") {
+		$(this).attr("src", animate);
+		$(this).attr("data-state", "animate");
+	} else {
+		$(this).attr("src", still);
+		$(this).attr("data-state", "still");
+	}
+}
+
+
 //pushing elements to the screen
 $(document).on("click", ".topicsButton", displayInfo)
+$(document).on("click", ".image", gifAnimate)
+
 renderButtons()
     
